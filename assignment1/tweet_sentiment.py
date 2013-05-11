26b0dcd71333e21d84d754295fe1a5f16eaad5e3
Christopher Roach <vthakr@gmail.com>
2013-05-10 23:47:22 -0700

    both tweet_sentiment an term_sentiment scripts use the same codebase

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -5,18 +5,6 @@ import json
 from collections import defaultdict
 
 
-# A list of standard punctuation used as the default list of characters to
-# delete from a text's text before calculating its sentiment score.
-# The punctuation data structure must be a dict where each character
-# is a key with a value of None. This is neccessary since the
-# unicode version of the translate method doesn't actually support the
-# deletechars keyword argument (as the ascii version does). Instead, it
-# will remove characters from unicode string if the value for that
-# character is None.
-punctuation=dict.fromkeys([i for i in xrange(sys.maxunicode)
-    if unicodedata.category(unichr(i)).startswith('P') and
-    unichr(i) != u"'"])  # ignore apostrophes to allow contractions
-
 def load_tweets(filename):
     """Loads the tweets file into a python list"""
     tweets = []
@@ -29,17 +17,45 @@ def load_sentiments(filename):
     sentiments = defaultdict(float)
     with open(filename) as fin:
         for line in fin:
+            # Skip commented out lines
+            if line.strip().startswith('#'):
+                continue
             word, sentiment = re.split('\t+', line)
             sentiments[word] = float(sentiment)
     return sentiments
+
+def split_words(tweet):
+    """Splits the text for the given tweet into a list of words
+
+    This function will first remove all of the punctuation in the text before
+    splitting the text into a list of words.
+
+    """
+    # The punctuation data structure must be a dict where each character
+    # is a key with a value of None. This is neccessary since the
+    # unicode version of the translate method doesn't actually support the
+    # deletechars keyword argument (as the ascii version does). Instead, it
+    # will remove characters from unicode string if the value for that
+    # character is None.
+    #
+    # The punctuation dict is cached on the function object since its creation
+    # can be expensive when done repeatedly for every tweet.
+    punctuation = getattr(split_words, "_punctuation", None)
+    if punctuation is None:
+        punctuation=dict.fromkeys([i for i in xrange(sys.maxunicode)
+            if unicodedata.category(unichr(i)).startswith('P') and
+            unichr(i) != u"'"])  # ignore apostrophes to allow contractions
+        setattr(split_words, "_punctuation", punctuation)
+
+    return tweet.get('text', u'').lower().translate(punctuation).split()
 
 def main(sentiment_filename, tweet_filename):
     sentiments = load_sentiments(sentiment_filename)
     tweets = load_tweets(tweet_filename)
     for tweet in tweets:
         # Remove the punctuation and split into words
-        words = tweet.get('text', u'').translate(punctuation).split()
-        score = sum(sentiments['word'] for word in words)
+        words = split_words(tweet)
+        score = sum(sentiments[word] for word in words)
         print float(score)
 
 
